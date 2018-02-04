b7aabb3934e13b32e4f7ef9f8ee1ed0453065c6f
goldnead <adrian.goldner@gmail.com>
2018-02-04 21:32:38 +0100

    build dist

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,8 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js/app.js')
+//  .js('resources/assets/js/app.js', 'public/fundraiser/js/app.js')
+//    .sass('resources/assets/sass/app.scss', 'public/fundraiser/css/app.css')
   .sass('resources/assets/sass/app.scss', 'public/css/app.css')
     .sourceMaps();
 
