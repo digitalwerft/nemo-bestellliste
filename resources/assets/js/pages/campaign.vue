ae3ed9d6216723bec5c2828ff2b2829053320f97
goldnead <adrian.goldner@gmail.com>
2018-04-12 01:11:24 +0200

    typo fix

--- resources/assets/js/pages/campaign.vue
+++ resources/assets/js/pages/campaign.vue
@@ -15,7 +15,7 @@
           <collector :collector-id="collector.id" :filterkey="search" v-on:delete-collector="onCollectorDelete(collector)" v-on:save-collector="onCollectorSave(collector)" v-on:editing-collector="handleEditing"></collector>
         </div>
         <div class="has-orders alert alert-warning mt-3 text-center" v-if="hasOrders">
-          <small>Hinweis: Dieses Projekt enthält bereits abgeschlossene Bestellungen. Um Alle Bestellungen zu sehen, rufe die <router-link :to="{ name: 'summary'}">Zusammenfassung</router-link> auf.</small>
+          <small>Hinweis: Dieses Projekt enthält bereits abgeschlossene Bestellungen. Um alle Bestellungen zu sehen, rufe die <router-link :to="{ name: 'summary'}">Zusammenfassung</router-link> auf.</small>
         </div>
         <div class="alert alert-no-collectors mt-3 text-muted text-center" v-if="filteredCollector && filteredCollector.length < 1">
           Deine Suche nach <mark>{{ search }}</mark> erzielte leider keine Treffer.
